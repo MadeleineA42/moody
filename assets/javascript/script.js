ed6de7647995b3c8dbaf826ee7a8adc5ab94969b
Marcus Matthews <mlmatthews75@gmail.com>
2023-08-14 19:54:58 -0400

    Mike added APIs to the quiz

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -252,4 +252,33 @@ function selectAnswer(button) {
         }
     }
 
+    const API_KEY = 'YOUR_API_KEY_HERE';
+const CHANNEL_ID = 'YOUR_CHANNEL_ID_HERE';
+
+// Endpoint to get uploads playlistId from a channel
+const CHANNEL_ENDPOINT = `https://www.googleapis.com/youtube/v3/channels?id=${CHANNEL_ID}&part=contentDetails&key=${API_KEY}`;
+
+// Fetch uploads playlistId from the channel
+    fetch(CHANNEL_ENDPOINT)
+    .then(response => response.json())
+    .then(data => {
+    const playlistId = data.items[0].contentDetails.relatedPlaylists.uploads;
+
+    // Endpoint to get videos from the uploads playlist
+    const PLAYLIST_ITEMS_ENDPOINT = `https://www.googleapis.com/youtube/v3/playlistItems?playlistId=${playlistId}&maxResults=5&part=snippet&key=${API_KEY}`;
+
+    return fetch(PLAYLIST_ITEMS_ENDPOINT);
+  })
+    .then(response => response.json())
+    .then(data => {
+    console.log(data.items);  // Logs the videos in the uploads playlist
+  })
+    .catch(error => {
+    console.error('Error fetching from YouTube API', error);
+  });
+  
+    function showResults() {
+        endResults.style.display = "block";
+    }
+
 }
