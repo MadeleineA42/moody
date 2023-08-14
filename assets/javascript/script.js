7772a59b6e5db93eb8186c73fb9b4081e06b086c
Marcus Matthews <mlmatthews75@gmail.com>
2023-08-14 19:48:51 -0400

    created points for each mood and set them to 0

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,3 +8,13 @@ let sadResult = document.getElementById("sadResult");
 let stressedResult = document.getElementById("stressedResult");
 let lovedResult = document.getElementById("lovedResult");
 let angryResult = document.getElementById("angryResult");
+
+var questionIndex = 0;
+
+startBtn.addEventListener("click", startQuiz);
+
+var happyPoints = 0;
+var sadPoints = 0;
+var stressedPoints = 0;
+var lovedPoints = 0;
+var angryPoints = 0;
