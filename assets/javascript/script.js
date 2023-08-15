4d27361dd25126eea160d2511c112ca5d5e5f726
kwaters3 <knickler3@gmail.com>
2023-08-15 17:30:41 -0400

    debugging

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -212,7 +212,7 @@ function selectAnswer(button) {
     function getMoodRating() {
         if (happyPoints >= 3) {
             console.log("You're Happy!");
-            happyResult.innerHTML = '<a href="./src/happy.html"><img src="../assets/images/happy-cow.png" alt="Happy MOOdy" class="max-w-full h-auto w-64"></a>';
+            happyResult.innerHTML = '<a href="./src/happy.html"><img src="./assets/images/happy-cow.png" alt="Happy MOOdy" class="max-w-full h-auto w-64"></a>';
             sadResult.innerHTML = ""
             stressedResult.innerHTML = ""
             lovedResult.innerHTML = ""
@@ -221,7 +221,7 @@ function selectAnswer(button) {
         if (sadPoints >= 3) {
             console.log("You're Sad.");
             happyResult.innerHTML = ""
-            sadResult.innerHTML = '<a href="./src/sad.html"><img src="../assets/images/sad-cow.png" alt="Sad MOOdy" class="max-w-full h-auto w-64"></a>';
+            sadResult.innerHTML = '<a href="./src/sad.html"><img src="./assets/images/sad-cow.png" alt="Sad MOOdy" class="max-w-full h-auto w-64"></a>';
             stressedResult.innerHTML = ""
             lovedResult.innerHTML = ""
             angryResult.innerHTML = ""
@@ -230,7 +230,7 @@ function selectAnswer(button) {
             console.log("You're Stressed!");
             happyResult.innerHTML = ""
             sadResult.innerHTML = ""
-            stressedResult.innerHTML = '<a href="./src/stressed.html"><img src="../assets/images/stressed-cow.png" alt="Stressed MOOdy" class="max-w-full h-auto w-64"></a>';
+            stressedResult.innerHTML = '<a href="./src/stressed.html"><img src="./assets/images/stressed-cow.png" alt="Stressed MOOdy" class="max-w-full h-auto w-64"></a>';
             lovedResult.innerHTML = ""
             angryResult.innerHTML = ""
         }
@@ -239,7 +239,7 @@ function selectAnswer(button) {
             happyResult.innerHTML = ""
             sadResult.innerHTML = ""
             stressedResult.innerHTML = ""
-            lovedResult.innerHTML = '<a href="./src/loved.html"><img src="../assets/images/loved-cow.png" alt="Loved MOOdy" class="max-w-full h-auto w-64"></a>';
+            lovedResult.innerHTML = '<a href="./src/loved.html"><img src="./assets/images/loved-cow.png" alt="Loved MOOdy" class="max-w-full h-auto w-64"></a>';
             angryResult.innerHTML = ""
         }
         if (angryPoints >= 3) {
@@ -248,7 +248,7 @@ function selectAnswer(button) {
             sadResult.innerHTML = ""
             stressedResult.innerHTML = ""
             lovedResult.innerHTML = ""
-            angryResult.innerHTML = '<a href="./src/angry.html"><img src="../assets/images/angry-cow.png" alt="Angry MOOdy" class="max-w-full h-auto w-64"></a>';
+            angryResult.innerHTML = '<a href="./src/angry.html"><img src="./assets/images/angry-cow.png" alt="Angry MOOdy" class="max-w-full h-auto w-64"></a>';
         }
     }
 
